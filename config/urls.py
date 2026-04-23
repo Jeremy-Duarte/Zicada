a2ae3cb15163077a898113e47c4a5ae486b36810
Jeremy-Duarte <jeremydamian.db@gmail.com>
2026-04-22 23:21:28 -0500

    build: construir urls para las vistas
    - Se construyen urls para las vistas en las app core, products y config para la landing page

--- config/urls.py
+++ config/urls.py
@@ -16,9 +16,18 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from apps.core.views import home, pwa_manifest
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home, name='home'),
+    path('manifest.json', pwa_manifest, name='pwa_manifest'),
+    path('core/', include('apps.core.urls')),
     path('orders/', include('apps.orders.urls')),
     path('products/', include('apps.products.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
