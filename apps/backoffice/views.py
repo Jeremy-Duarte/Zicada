ea01a188ab9d1e558dc8d01bc966783d8fa8c744
Jeremy-Duarte <jeremydamian.db@gmail.com>
2026-05-10 17:04:58 -0500

    chore(backoffice): vistas dummy
    - Se crean vistas dummy para los indexes de productos y dashboard de administrador

--- apps/backoffice/views.py
+++ apps/backoffice/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from django.contrib.admin.views.decorators import staff_member_required
 
-# Create your views here.
+@staff_member_required
+def admin_dashboard(request):
+    context = {'section': 'dashboard'}
+    return render(request, 'backoffice/admin_dashboard.html', context)
+
+@staff_member_required
+def admin_orders(request):
+    context = {'section': 'orders'}
+    return render(request, 'backoffice/admin_orders.html', context)
+
+@staff_member_required
+def admin_products(request):
+    context = {'section': 'products'}
+    return render(request, 'backoffice/admin_products.html', context)
+
+@staff_member_required
+def admin_users(request):
+    context = {'section': 'users'}
+    return render(request, 'backoffice/admin_users.html', context)
+
+@staff_member_required
+def admin_config(request):
+    context = {'section': 'config'}
+    return render(request, 'backoffice/admin_config.html', context)
