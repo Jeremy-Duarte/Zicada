f222fdb6ba93a18b108de9004e92fd51eddf7447
Jeremy-Duarte <jeremydamian.db@gmail.com>
2026-05-13 10:53:13 -0500

    build(config): agregar app users a rutas urls
    - Para agregar redirecciones a futuro en la app usuarios

--- config/urls.py
+++ config/urls.py
@@ -27,6 +27,7 @@ urlpatterns = [
     path('core/', include('apps.core.urls')),
     path('orders/', include('apps.orders.urls')),
     path('products/', include('apps.products.urls')),
+    path('users/', include('apps.users.urls')),
     path('backoffice/', include('apps.backoffice.urls')),
 ]
 
