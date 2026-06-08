96022093510c889a123add0c4f8b7a8776a5d60f
Jeremy-Duarte <jeremydamian.db@gmail.com>
2026-06-08 09:49:58 -0500

    build(core): quitar filtros no usados
    - Resolver TODO
    - No se requieren filtros avanzados para esta etapa del proyecto

--- apps/core/crud/filters.py
+++
@@ -1,6 +0,0 @@
-# Puede ser un complemento para usar django-filter, pero por ahora no es necesario.
-# Si se quiere integración con django-filter, se puede agregar luego.
-
-class BaseFilterSet:
-    """Placeholder para futura integración con django-filter.""" #TODO
-    pass
