15d97b31b47d202c556899c73b87a927c214536e
Jeremy-Duarte <jeremydamian.db@gmail.com>
2026-06-16 16:18:53 -0500

    chore(config): instalar humanize de contrib

--- config/settings.py
+++ config/settings.py
@@ -24,6 +24,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     # Vendor Apps
     'rest_framework',
+    'django.contrib.humanize',
     'cloudinary',
     'cloudinary_storage',
     'django_crontab',
