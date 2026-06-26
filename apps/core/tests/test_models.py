2bd438abf959cf2359751fd0eb63a21ff1a2e77d
Jeremy-Duarte <jeremydamian.db@gmail.com>
2026-06-25 21:20:54 -0500

    test(core): test unitario 001 al 004
    - Se formulan los 4 test
    - Pruebas exitosamente ejecutadas

--- apps/core/tests/test_models.py
+++ apps/core/tests/test_models.py
@@ -1,18 +1,42 @@
 import pytest
+from apps.products.models import Product
 
-class TestActiveMangere:
+@pytest.mark.django_db
+class TestActiveManager:
     # UT - 001
-    def test_returns_active_objetcs():
-        pass
-    # UT - 002
-    def test_returns_filtered_objetcs():
-        pass
+    def test_returns_active_objetcs(self, product_whit_state):
+        product_active = product_whit_state(True)
+        product_inactive = product_whit_state(False)
+        result = Product.objects.all()
 
+        assert product_active in result
+        assert product_inactive not in result
+        assert result.count() == 1
+    # UT - 002
+    def test_returns_filtered_objetcs(self, product_whit_state):
+        product_active = product_whit_state(True)
+        product_inactive = product_whit_state(False)
+        result = Product.all_objects.all()
+
+        assert product_active in result
+        assert product_inactive in result
+        assert result.count() == 2
+
+@pytest.mark.django_db
 class TestBaseAuditModel:
     # UT - 003
-    def test_models_can_be_softdeleted():
-        pass
+    def test_models_can_be_softdeleted(self, base_product):
+        product = base_product
+        product.soft_delete()
 
+        assert product.is_active == False
+        assert product.deleted_at is not None
+        
     #UT - 004
-    def test_softdeleted_models_can_be_restored():
-        pass
+    def test_softdeleted_models_can_be_restored(self, base_product):
+        product = base_product
+        product.soft_delete()
+        product.restore()
+
+        assert product.is_active == True
+        assert product.deleted_at is None
